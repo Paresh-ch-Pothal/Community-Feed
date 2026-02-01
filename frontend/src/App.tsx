a07bb2a74a1b55a0dba4a3b4b95bef3d7118c5a4
Paresh-ch-Pothal <pareshchandrapothal44@gmail.com>
2026-02-01 20:37:36 +0530

    Vercel file has been updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Signup from './components/signup/signup';
-import Login from './components/login/login.';
-import Feed from './components/feed/feed';
+import Signup from './components/signup/Signup';
+import Login from './components/login/Login.';
+import Feed from './components/feed/Feed';
+
+
 
 
 const App: React.FC = () => {
